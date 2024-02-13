f4d4674a61c1fcc586924bb9b29e0a00c107a514
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-02-13 19:29:09 +0000

    Release 8.0.0-beta

--- src/metriport/core/client_wrapper.py
+++ src/metriport/core/client_wrapper.py
@@ -14,7 +14,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "metriport",
-            "X-Fern-SDK-Version": "8.0.0",
+            "X-Fern-SDK-Version": "8.0.0-beta",
         }
         headers["X-API-Key"] = self.api_key
         return headers
